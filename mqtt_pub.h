14605cbc6249e772fbc6ace6b4530cf1a90e9b56
sundeep471 <sundeep471@gmail.com>
2024-08-13 16:17:35 -0500

    Create mqtt_pub.h
    mqtt class header file for MQTT broker

---
+++ mqtt_pub.h
@@ -0,0 +1,41 @@
+#include <mosquitto.h>
+
+class mqtt {
+public:
+    struct mosquitto *mosq;
+
+    void init(){
+        mosquitto_lib_init();
+        mosq = mosquitto_new("publisher-test", true, NULL);
+        int rc = mosquitto_connect(mosq, "localhost", 1990, 60);
+        if (rc != MOSQ_ERR_SUCCESS) {
+            std::cerr << "Client could not connect to broker! Error Code: " << rc << "\n";
+            mosquitto_destroy(mosq);
+        } else {
+            std::cout << "We are now connected to the broker!\n";
+        }
+    }
+
+    void publish(const std::string& msg, const std::string& topic, int len) {
+        if (mosq == nullptr) {
+            std::cerr << "Mosquitto client not initialized.\n";
+            return;
+        }
+
+        //mosquitto_publish(mosq, NULL, "test/t1", 6, "Hello", 0, false);
+        //mosquitto_publish(mosq, nullptr, topic.c_str(), len, msg.c_str(), 0, false);
+        int rc = mosquitto_publish(mosq, nullptr, topic.c_str(), len, msg.c_str(), 0, false);
+        if (rc != MOSQ_ERR_SUCCESS) {
+            std::cerr << "Failed to publish message. Error Code: " << rc << "\n";
+        }
+    }
+
+    ~mqtt() {
+        if (mosq != nullptr) {
+            mosquitto_disconnect(mosq);
+            mosquitto_destroy(mosq);
+        }
+
+        mosquitto_lib_cleanup();
+    }
+};
